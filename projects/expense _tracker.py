0fa2937554326d86e2de909220810342b518ee97
Tannu531 <tdahiya090@gmail.com>
2026-01-23 01:04:43 +0530

    Update expense _tracker.py

--- projects/expense _tracker.py
+++ projects/expense _tracker.py
@@ -1,35 +1,42 @@
 print("MENU")
 
-1="Add expense"
+option1="Add expense"
 print(1)
-2="View expenses"
+option2="View expenses"
 print(2)
-3="Category wise total"
+option3="Category wise total"
 print(3)
-4="EXIT"
+option4="EXIT"
 print(4)
+option1==1
+option2==2
+option3==3
+option4==4
 
-n=input("Enter user choice: ")
-o=n
-if(o==1):
-    a=int(input("add expense"))
-    Expenses=[]
-    L=(input("enter new expense: "))
-    Expenses.append(L)
-    d={"amount" : int(input("Amount")),
+d={"amount" : int(input("Amount")),
    "category" : input("category"),
    "note" : input("note") ,
    "Date" : input("Date") }
-    print(a)
+Expenses=[d.get("amount")]
+n=int(input("Enter user choice: "))
 
-elif(o==2):
-    t="Date","category","amount","note"    
-    print(t)
+if(n==1):
+    L=(input("enter expense: "))
+    A=int(input("add expense"))
     
-elif(o==3):
-    b=int(input(f"amount spend on {"note"}"))
-    print(b)
-elif(o==4):
+    Expenses.append(A)
+    print(Expenses)
+    print(d)
+
+elif(n==2):
+    print(d.get("amount")),
+    print(d.get("category")),
+    print(d.get("note")),
+    print(d.get("Date"))    
+elif(n==3):
+    pass
+    
+elif(n==4):
     print("EXIT")        
 
 

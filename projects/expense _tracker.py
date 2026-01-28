77435927904bab45a2857685bf07e08d9fdf4448
Tannu531 <tdahiya090@gmail.com>
2026-01-28 22:44:36 +0530

    Set up expense tracker structure using list of dictionaries

--- projects/expense _tracker.py
+++ projects/expense _tracker.py
@@ -1,51 +1,55 @@
 print("MENU")
 
-
-option1="Add expense"
 print("1.add expense")
-option2="View expenses"
 print("2.view expense")
-option3="Category wise total"
 print("3.category wise total")
-option4="EXIT"
 print("4.exit")
 
-d={}
+
 Expenses=[]
+
 
 while True:
  n=int(input("Enter user choice: "))
  if(n==1):
     
-    d1={"amount" : int(input("Amount")),
-   "category" : input("category"),
-   "note" : input("note") ,
-   "Date" : input("Date") }
+    expense={"amount" : int(input("Amount: ")),
+   "category" : input("category: "),
+   "note" : input("note: ") ,
+   "Date" : input("Date: ")
+     }
 
-    Expenses.append(d1)
+    Expenses.append(expense)
     print(Expenses)
-    
-
+ 
  elif(n==2):
-    for d in Expenses:
-     print(d.get("amount")),
-     print(d.get("category")),
-     print(d.get("note")),
-     print(d.get("Date"))    
+    if not Expenses:    
+     print("no expenses added yet,please add an expense first")
+    else: 
+     for e in Expenses:
+      print("amount:",e["amount"]),
+      print("category:",e["category"]),
+      print("note:",e["note"])
+      print("Date:",e["Date"])
+      print("------")
  elif(n==3):
-    T=input("user category")
-    total=0
-    for e in Expenses:
-      if e["category"]==T:
-        total=total+["amount"]
-        print(total)
+     if not Expenses:
+      print("No expenses added yet , please add an expense first")  
+     else:    
+      T=input("user category: ")
+      total=0
+      for e in Expenses:
+       if e["category"].lower()==T.lower():
+        total=total+e["amount"]
+     print("Total expense for",T,"=",total)    
       
 
    
  elif(n==4):
     print("EXIT ,Thanks!")  
     break      
-
+ else:
+   print("invalid choice enter choice 1-4")
 
 
 

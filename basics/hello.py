ef05f62cae63bb4400ca2b4da22de2cc3a8da2b5
Tannu531 <tdahiya090@gmail.com>
2026-02-09 22:27:21 +0530

    Update hello.py

--- basics/hello.py
+++ basics/hello.py
@@ -1,11 +1,14 @@
-def prime_no(n):
-    for i in range(2,n):
-     if(n==0 or n==1):
-        return
-     elif(n%i==0):
-        print("number is not prime")
-        break
-     else: 
-        print("number is prime")  
-n=int(input("enter your number: "))
-print=(f"sum of {n} natural numbers is: ",prime_no(n))    
+a="31.2"
+b=float(a)
+t=type(b)
+print(t)
+
+name=input("enter your name:")
+print(f"my name is {name}")
+
+fruits=[]
+f1=input("enter fruit name: ")
+fruits.append(f1)
+f2=input("enter fruit name: ")
+fruits.append(f2)  #columns are important after append 
+print(fruits)

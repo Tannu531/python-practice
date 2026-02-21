dde6fab017059518a35fd9261070ac829d3cea06
Tannu531 <tdahiya090@gmail.com>
2026-02-21 21:57:19 +0530

    Update jarvis.py

--- projects/jarvis.py
+++ projects/jarvis.py
@@ -1,16 +1,42 @@
 import webbrowser
-n=input("enter your command: ")
 import pyttsx3
 object=pyttsx3.init()
-
-
-if ("youtube" in n):
+object.say("hello, how can i help you ")
+object.runAndWait()
+while(True):
+ n=input("enter your command: ").lower()
+ if("exit" in n or "stop"in n or "bye" in n):
+    object.say("goodbye")
+    object.runAndWait()
+    break
+ elif("youtube" in n):
     object.say("opening youtube")
-    object.run()
-    open ("youtube")
-elif("google" in n):
+    object.runAndWait()
+    webbrowser.open("https://www.youtube.com")
+    
+ elif("google" in n):
     object.say("opening google")
-    object.run()
-    open ("google")    
-else:
-    print("i dont understand")    
+    object.runAndWait()
+    webbrowser.open("https://www.google.com")  
+ elif("whatsapp" in n):
+    object.say("opening whatsapp")   
+    object.runAndWait()
+    webbrowser.open("https://web.whatsapp.com")
+ elif("github" in n):
+    object.say("opening github")   
+    object.runAndWait()
+    webbrowser.open("https://github.com")   
+ elif("spotify" in n):
+    object.say("opening spotify")   
+    object.runAndWait()
+    webbrowser.open("https://open.spotify.com")   
+ elif("instagram" in n):
+    object.say("opening instagram")   
+    object.runAndWait()
+    webbrowser.open("https://www.instagram.com")  
+ elif("time" in n):
+    pass
+              
+   
+ else:
+    object.say("i dont understand")    
